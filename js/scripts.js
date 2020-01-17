85045208de5549f1f779f6246d5dd51ff59ed8d1
brevin <cronkbrevin98@gmail.com>
2020-01-17 14:00:39 -0800

    6th commit still working on JS

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $("form#quiz").sumbit(function(event){
+        event.preventDefault();
+        var c1=  $("#c1").val()
+        var c2 = $("#c2").val()
+        var c3 = $("#c3").val()
+        var c4 =$("#c4").val()
+    
+    });
+});
+
+if
